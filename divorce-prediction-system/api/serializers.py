b1a121ebc6f7c1efc1cc78eb9ba204d00ab2745c
Kenechukwu Philip Adigwe <66175753+kaycee328@users.noreply.github.com>
2024-09-08 23:36:37 +0100

    code refactoring

--- divorce-prediction-system/api/serializers.py
+++ divorce-prediction-system/api/serializers.py
@@ -12,9 +12,3 @@ class DpsSerializer(serializers.ModelSerializer):
     class Meta:
         model = DPS
         fields = ["pk", "username", "divorced"] + [f"n{i}" for i in range(1, 53)]
-
-    # def create(self, validated_data):
-    #     # Automatically set the 'user' field to the currently logged-in user
-    #     user = self.context["request"].user
-    #     validated_data["user"] = user
-    #     return super(DpsSerializer, self).create(validated_data)
