1866d8d8a554d5e7d56e6e558fc1ff3f41272f74
Kenechukwu Philip Adigwe <66175753+kaycee328@users.noreply.github.com>
2024-09-14 18:36:36 +0100

    Update serializers.py
    Import code from another project

--- divorce-prediction-system/users/serializers.py
+++ divorce-prediction-system/users/serializers.py
@@ -1,8 +1,9 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 
-class CreateUserSerializer(ModelSerializer):
+class CreateUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["username", "password"]
@@ -12,3 +13,26 @@ class CreateUserSerializer(ModelSerializer):
             username=validated_data["username"], password=validated_data["password"]
         )
         return user
+
+
+class LoginUserSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(
+        write_only=True
+    )  # write_only ensures password isn’t exposed
+
+    def validate(self, data):
+        username = data.get("username")
+        password = data.get("password")
+
+        if username and password:
+            # Authenticate user
+            user = authenticate(username=username, password=password)
+            if user:
+                data["user"] = user  # Pass the user in the validated data
+            else:
+                raise serializers.ValidationError("Invalid login credentials.")
+        else:
+            raise serializers.ValidationError("Username and password are required.")
+
+        return data
