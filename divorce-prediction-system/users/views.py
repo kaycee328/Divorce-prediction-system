97dc29c7b5eabe4092c97fe87a2b1ba538ff06cd
Kenechukwu Philip Adigwe <66175753+kaycee328@users.noreply.github.com>
2024-12-02 13:57:04 +0100

    Update views.py
    Updatesd the logout function according to the syntax of django 5.0

--- divorce-prediction-system/users/views.py
+++ divorce-prediction-system/users/views.py
@@ -6,29 +6,39 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 
+
 # This view handles user sign up authentication
 def signupPage(request):
     form = UserRegisterForm()
 
-    if request.method == 'POST': #executes this code block if the form sends a POST request
+    if (
+        request.method == "POST"
+    ):  # executes this code block if the form sends a POST request
         form = UserRegisterForm(request.POST)
 
-        if form.is_valid(): #this code block runs if the form values are valid
+        if form.is_valid():  # this code block runs if the form values are valid
             form.save()
-            return redirect('signin')
-        
-    context = {'form': form}
-    return render(request, 'users_app/auth/signup.html', context)
+            return redirect("signin")
+
+    context = {"form": form}
+    return render(request, "users_app/auth/signup.html", context)
+
 
 # This view handles user login authentication
 class UserLogin(LoginView):
-    template_name = 'users_app/auth/signin.html'
-    fields = '__all__'
+    template_name = "users_app/auth/signin.html"
+    fields = "__all__"
     redirect_authenticated_user = True
 
-    def get_success_url(self) -> str: #this is the url that should load when the user successfully logs in
-        return reverse_lazy('homepage')
+    def get_success_url(
+        self,
+    ) -> str:  # this is the url that should load when the user successfully logs in
+        return reverse_lazy("homepage")
+
 
 # This view handles user logout
-class Logout_user(LogoutView):
-    template_name = 'users_app/auth/logout.html'
+# class Logout_user(LogoutView):
+#     template_name = 'users_app/auth/logout.html'
+def logout_view(request):
+    logout(request)
+    return redirect("signin1")
